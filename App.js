cd6c126335656052d474e013a1ef6028a2081335
Steve Faulkner <stfaul@microsoft.com>
2020-11-12 22:00:38 -0600

    Fix Data Loading

--- App.js
+++ App.js
@@ -5,52 +5,53 @@
  * @format
  * @flow strict-local
  */
+import 'react-native-get-random-values';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {CosmosClient} from '@azure/cosmos';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-  ActivityIndicator,
-  FlatList,
-  TouchableOpacity,
-  useState, useEffect
-} from 'react-native';
+const endpoint = 'https://amos-cosmosdb.documents.azure.com:443/';
+const key =
+  'k8rrY0TEv8XvllYkJ4PlhSanMkTa9eAQxJjt146Ad5z9RbzDLfjlw8QSVVNUDuqZGniBPmYxIMBYhhSE3h8gJg==';
+const databaseId = 'ToDoList';
+const containerId = 'Items';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const CosmosClient = require('@azure/cosmos').CosmosClient
-
-const endpoint = 'https://amos-cosmosdb.documents.azure.com:443/'
-const key = 'k8rrY0TEv8XvllYkJ4PlhSanMkTa9eAQxJjt146Ad5z9RbzDLfjlw8QSVVNUDuqZGniBPmYxIMBYhhSE3h8gJg=='
-const databaseId = 'ToDoList'
-const containerId = 'Items'
-
-const client = new CosmosClient({ endpoint, key })
+const client = new CosmosClient({endpoint, key});
 
 const querySpec = {
-  query: 'SELECT * from c'
+  query: 'SELECT * from c',
 };
 
-var loading = true
+var loading = true;
 
 const App: () => React$Node = () => {
-  var results = client.database(databaseId).container(containerId).items.query(querySpec);
-  var resultsString = JSON.stringify(results)
+  const [todos, setTodos] = useState();
+  useEffect(() => {
+    client
+      .database(databaseId)
+      .container(containerId)
+      .items.query(querySpec)
+      .fetchAll()
+      .then((response) => {
+        setTodos(response.resources);
+      });
+  });
+  if (!todos) {
+    return (
+      <View style={styles.container}>
+        <Text>Loading Todos...</Text>
+      </View>
+    );
+  }
+  console.log(todos);
   return (
     <View style={styles.container}>
-      <Text>{resultsString}</Text>
+      {todos.map((todo) => {
+        return <Text>{todo.name}</Text>;
+      })}
     </View>
-  )
+  );
 };
 
 const styles = StyleSheet.create({
